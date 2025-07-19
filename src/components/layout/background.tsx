ecdf0d720a7428f479fde8583316c0ae0ed6ef45
INVESTED505 Passive Income Ideas <invested505@gmail.com>
2025-07-19 19:01:19 +0000

    you can see  it more now in the image but it still is not as visible as

--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -1,15 +1,10 @@
 export function Background() {
   return (
     <div
-      className="fixed inset-0 -z-20 h-full w-full bg-[#14171f]"
+      className="fixed inset-0 -z-20 h-full w-full bg-[hsl(var(--background))]"
       aria-hidden="true"
     >
-      <div className="absolute inset-0 -z-10 bg-gradient-to-br from-transparent via-black/50 to-black"></div>
-      
-      <div className="absolute top-1/2 left-1/2 h-[600px] w-[600px] rounded-full bg-[radial-gradient(circle_at_center,hsl(var(--primary)/0.25)_0%,transparent_60%)] opacity-80 blur-3xl transform -translate-x-[40%] -translate-y-1/2"></div>
-
-      <div className="absolute bottom-0 right-0 h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_at_center,hsl(var(--accent)/0.1)_0%,transparent_70%)] opacity-60 blur-3xl transform translate-x-1/3 translate-y-1/3"></div>
-
+      <div className="absolute inset-0 z-0 bg-gradient-to-br from-[#2b0f0a] via-[hsl(var(--background))] to-[hsl(var(--background))]"></div>
     </div>
   );
 }
