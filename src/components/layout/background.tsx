99ab82d03828998b52f0251344e929fc9f4a232d
INVESTED505 Passive Income Ideas <invested505@gmail.com>
2025-07-19 18:58:48 +0000

    almost there, but now you need to  scale of the light parts of the gradi

--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -6,7 +6,7 @@ export function Background() {
     >
       <div className="absolute inset-0 -z-10 bg-gradient-to-br from-transparent via-black/50 to-black"></div>
       
-      <div className="absolute top-1/2 left-1/2 h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_at_center,hsl(var(--primary)/0.15)_0%,transparent_70%)] opacity-70 blur-3xl transform -translate-x-[40%] -translate-y-1/2"></div>
+      <div className="absolute top-1/2 left-1/2 h-[600px] w-[600px] rounded-full bg-[radial-gradient(circle_at_center,hsl(var(--primary)/0.25)_0%,transparent_60%)] opacity-80 blur-3xl transform -translate-x-[40%] -translate-y-1/2"></div>
 
       <div className="absolute bottom-0 right-0 h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_at_center,hsl(var(--accent)/0.1)_0%,transparent_70%)] opacity-60 blur-3xl transform translate-x-1/3 translate-y-1/3"></div>
 
